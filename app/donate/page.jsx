36084f18f275041092496866ec4c88e6d7b2c8d3
ilorde-exe <92220271+ilorde-exe@users.noreply.github.com>
2024-04-07 00:51:53 +0530

    killing myself today ;)

--- app/donate/page.jsx
+++ app/donate/page.jsx
@@ -1,15 +1,38 @@
 "use client";
 import React from "react";
+import { useState } from "react";
 
 const page = () => {
+  const [input, setInput] = useState("");
   return (
     <div>
       <div class="font-[sans-serif] text-[#333]">
         <div class="min-h-screen flex flex-col items-center justify-center py-6 px-4">
-          <div class="max-w-md w-full border py-8 px-6 rounded border-gray-300 bg-white">
+          <div class="max-w-md w-full border py-8 px-6 rounded-xl border-gray-300 bg-white">
             <h2 class="text-center text-3xl font-extrabold">
               Enter all the neccessary details
             </h2>
+            <br />
+            {input ? (
+              <iframe
+                width="360"
+                height="375"
+                loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"
+                src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyBHgskmG3f7puRGh6avIDMryMKu8w7oR2M&&q=${encodeURIComponent(
+                  input
+                )}`}
+              ></iframe>
+            ) : (
+              <iframe
+                width="360"
+                height="375"
+                loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"
+                src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyBHgskmG3f7puRGh6avIDMryMKu8w7oR2M&&q=india`}
+              ></iframe>
+            )}
+
             <form class="mt-10 space-y-4">
               <div>
                 <input
@@ -34,6 +57,9 @@ const page = () => {
                   class="py-3 px-4 block w-full border-2 border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
                   rows="3"
                   placeholder="Address"
+                  onChange={(e) => {
+                    setInput(e.target.value);
+                  }}
                 ></textarea>
               </div>
               <div class="!mt-10">
