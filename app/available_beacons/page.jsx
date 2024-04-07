a4831c3147777d35b953ad3bad82ad9d42cb5cab
Priyavrata Mohan <91372958+priyavratamohan@users.noreply.github.com>
2024-04-07 07:50:36 +0530

    Create page.jsx

---
+++ app/available_beacons/page.jsx
@@ -0,0 +1,11 @@
+import react from "react";
+
+const page = () => {
+    return(
+        <div class="bg-yellow-200 h-screen w-screen  justify-center items-center">
+                <h1 class="text-center text-3xl font-extrabold font-serif">Replating</h1>        
+        </div>
+    )
+}
+
+export default page;
